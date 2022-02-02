8654d632d13af5f5c6e936b45ccf5fe36d5ba551
truonghoangthuan <tthun32@gmail.com>
2022-02-02 16:03:58 +0700

    Initialized

---
+++ src/main/java/com/thuan/coronavirustracker/CoronaVirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.thuan.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVirusTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaVirusTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thuan/coronavirustracker/CoronaVirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuan.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
